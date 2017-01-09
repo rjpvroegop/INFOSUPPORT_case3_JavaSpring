7dc81d2153d28b7155099c20134d3978f22098ca
Maarten <info@javaminor2015.com>
2017-01-09 15:49:43 +0100

    fixed addresses fixed customer-address relation

--- Bsklantbeheer/src/main/java/com/infosupport/domain/Address.java
+++ Bsklantbeheer/src/main/java/com/infosupport/domain/Address.java
@@ -3,6 +3,7 @@ package com.infosupport.domain;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,6 +16,7 @@ import javax.persistence.Id;
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class Address {
 
