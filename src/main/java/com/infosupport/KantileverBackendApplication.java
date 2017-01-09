28dd5a23034baa1ddc1faf968ca8f86a3ca4fb85
Yusuf <info@javaminor2015.com>
2017-01-09 10:02:08 +0100

    Kantilever Backend setup

---
+++ src/main/java/com/infosupport/KantileverBackendApplication.java
@@ -0,0 +1,12 @@
+package com.infosupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KantileverBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KantileverBackendApplication.class, args);
+	}
+}
