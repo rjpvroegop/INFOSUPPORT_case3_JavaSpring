bf4ce87e316ca262aa701ad621120a52af509756
dreaneii <kdominicus@gmail.com>
2017-01-11 11:05:37 +0100

    Hotfix: Cross origin toegevoegd voor de bs klantbeheer

--- Bsklantbeheer/src/main/java/com/infosupport/resource/AddressResource.java
+++ Bsklantbeheer/src/main/java/com/infosupport/resource/AddressResource.java
@@ -6,10 +6,7 @@ import com.infosupport.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,6 +15,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 /**
  * Created by maart on 10-1-2017.
  */
+@CrossOrigin
 @RestController
 @RequestMapping("address")
 public class AddressResource {
--- Bsklantbeheer/src/main/java/com/infosupport/resource/WebshopAccountResource.java
+++ Bsklantbeheer/src/main/java/com/infosupport/resource/WebshopAccountResource.java
@@ -6,10 +6,7 @@ import com.infosupport.service.WebshopAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -17,6 +14,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 /**
  * Created by maart on 10-1-2017.
  */
+@CrossOrigin
 @RestController
 @RequestMapping("accounts")
 public class WebshopAccountResource {
